772350fe7b8f58c18f9c8e2b350d8bbe39513020
Ayat <ayat.rashad@gmail.com>
2016-03-15 17:04:12 +0000

    using selenium and phantomjs

--- links_scraper.py
+++ links_scraper.py
@@ -2,20 +2,24 @@
 
 import re, logging, time, sys
 from  collections import deque
-from urlparse import urljoin
+from urlparse import urljoin, urlparse
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ActionChains
+from selenium.common import exceptions
 
 import numpy as np
 from bs4 import BeautifulSoup
 
 class LinksScraper:
     providers = [
-                 'http://finance.yahoo.com/news/provider-accesswire',
+                 #'http://finance.yahoo.com/news/provider-accesswire',
                  'http://finance.yahoo.com/news/provider-americancitybusinessjournals',
                  'http://finance.yahoo.com/news/provider-ap',
                  'http://finance.yahoo.com/news/provider-the-atlantic',
@@ -93,14 +97,14 @@ class LinksScraper:
         self.log.addHandler(fh)
         self.log.addHandler(ch)
 
-        self.retry = 5
-        self.links_file = open('external-links.txt', 'w')
+        self.retry = 10
+        # self.links_file = open('external-links.txt', 'w')
         self.recent_links = deque(maxlen=1000)
         self.maxduplicates = 300
-        self.timeout = 40
+        self.timeout = 80 
         self.YF_root = 'http://finance.yahoo.com'
 
-        #self.br = self.setup_browser()
+        self.br = self._setup_browser()
 
         # proxy list
         with open('proxies.txt') as f:
@@ -114,7 +118,7 @@ class LinksScraper:
 
         dcap = dict(DesiredCapabilities.PHANTOMJS)
         #dcap["phantomjs.page.settings.userAgent"] = (
-        #"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"
+        #"Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0"
         #)
         
         if prx:
@@ -124,7 +128,7 @@ class LinksScraper:
 
         br = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
         br.implicitly_wait(self.timeout)
-        driver.set_page_load_timeout(self.timeout) 
+        br.set_page_load_timeout(self.timeout) 
         
         return br
 
@@ -137,6 +141,8 @@ class LinksScraper:
                     
                 proxy = np.random.choice(self.proxies)
                 self.br = self._setup_browser(prx=proxy)
+                self.ac = ActionChains(self.br)
+                self.wait = WebDriverWait(self.br, self.timeout)
                 res = self.br.get(url)
                 return (True, res)
             
@@ -147,7 +153,6 @@ class LinksScraper:
     
 
     def scrape_news_links(self):
-        wait = WebDriverWait(self.br, 30)
         
         for provider_url in self.providers:
             succ, res = self._open_url(provider_url)
@@ -157,6 +162,7 @@ class LinksScraper:
                 continue                
                 
             self.log.info("Scraping provider: %s.." %provider_url)
+            self.links_file = open('%s.txt' %urlparse(provider_url).path[1:], 'w')
             retries = duplicates = 0
             last_title = ''
             self.recent_links.clear()
@@ -164,65 +170,96 @@ class LinksScraper:
             while True:
                 if retries > self.retry:
                     self.log.info('Retry limit exceeded...moving to next provider')
+                    self.links_file.close()
                     break
                 
                 #links = list(br.links(url_regex="bloomberg\.com|cnbc\.com|marketwatch\.com"))
-                links = self.br.find_elements_by_css_selector('.txt a')
+                links = self.br.find_elements_by_css_selector('.txt')
                 self.log.debug('#links found: %d' %len(links))
-
-                # test for repeated pages
-                if last_title and last_title == links[-1].text:
-                    self.log.debug('page repeated..')
-                    succ, res = self._open_url(provider_url)     # retry  with new proxy          
-                    if not succ:
-                        break
-                    last_title = ''
-                    retries += 1
-                    continue
-
-                last_title = links[-1].text 
                 
                 if len(links) == 0:
                     self.log.error('No Links found..')
 
                     with open('lastpage.html', 'w') as f:
-                        f.write(unicode(res).encode('utf-8'))
-                        self.log.info('failed..page contents written to lastpage.html')
-                    break
+                        f.write(unicode(self.br.page_source).encode('utf-8'))
+                        self.log.error('failed..page contents written to lastpage.html')
+
+                    succ, res = self._open_url(provider_url)     # retry  with new proxy          
+
+                    if not succ:
+                        self.links_file.close()
+                        break
+                    
+                    retries += 1
+                    continue
+                    
+                # test for repeated pages
+                if last_title and last_title == links[-1].find_element_by_tag_name('a').text:
+                    self.log.debug('page repeated..')
+                    #succ, res = self._open_url(provider_url)     # retry  with new proxy          
+                    #if not succ:
+                    #    break
+                    retries += 1
+                    #continue
+
+                last_title = links[-1].find_element_by_tag_name('a').text 
                 
                 for link in links:
-                    link = link.get_attribute('href')
+                    try:
+                        date = link.find_element_by_tag_name('cite')
+                        if date:
+                            date = date.text.split('-')[1]
+                    except Exception as e:
+                        self.log.debug(e)
+                        #print link.find_element_by_tag_name('cite').text
+                        date = ''
+                    link = link.find_element_by_tag_name('a').get_attribute('href')
 
-                    if link.startswith('/news'):        # local link
+                    if link.startswith('/news'):        # relative link
                         link = urljoin(self.YF_root, link)
                     
                     if link not in self.recent_links:
-                        self.links_file.write(link + '\n')
+                        self.links_file.write('%s, "%s" \n' %(link, date))
                         self.recent_links.append(link)
                     else:
                         duplicates += 1
-                        continue
 
                 self.log.debug('Duplicate-links so far: %d' %duplicates)
-                               
-                #if duplicates >= self.maxduplicates:
-                #    self.log.error('Duplicate-links limit exceeded..moving to next Provider')
-                #    break
 
-                # next page
-                elem = self.br.find_element_by_partial_link_text("Next >>")
-                elem.click()
+                # navigate to next page
+                try:
+                    #elem = self.br.find_element_by_partial_link_text("Next >>")
+                    elem = self.wait.until(
+                                EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.more-inline'))
+                                )
+                    self.ac.move_to_element(elem).perform()
+                    elem.click()
+                    
+                except exceptions.NoSuchElementException as e:
+                    self.log.error('Elem not found..%s' %e)
+                    
+                    with open('lastpage.html', 'w') as f:
+                        f.write(unicode(self.br.page_source).encode('utf-8'))
+                        
+                    self.br.get_screenshot_as_file('sshot.jpeg')
+                    
+                except TimeoutException as e:
+                    self.log.error('Click timeout..%s' %e)
+                    #retries += 1
+                    #continue
 
                 # wait untill javascript complete loading data
                 try:
-                    waitResult = wait.until(EC.staleness_of(links[0]))
-                except:
+                    waitResult = self.wait.until(EC.staleness_of(links[-1]))
+                except Exception as e:
+                    self.log.debug(e)
                     self.log.error('Script timeout..')
                     retries += 1
+                    self.log.debug('Retrying..')
                     continue
                 
-                self.log.info("Next Page...")                    
-                time.sleep(1)           # throttle
+                self.log.info("Next Page...")
+                time.sleep(5)           # throttle                
                 
 
 
